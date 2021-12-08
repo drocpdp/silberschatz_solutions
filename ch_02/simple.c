1a37624b842942f25c1ff2f96397cda049c520c3
drocpdp <davidreynon@gmail.com>
2021-12-08 12:30:42 +0100

    changes

--- ch_02/simple.c
+++ ch_02/simple.c
@@ -14,19 +14,22 @@
 #include <linux/kernel.h>
 #include <linux/hash.h>
 #include <linux/gcd.h>
+#include <linux/jiffies.h>
+#include <asm/param.h>
 
 /* This function is called when the module is loaded. */
 static int simple_init(void)
 {
-       printk(KERN_INFO "Loading Module\n");
-       printk(KERN_INFO "Golden Ratio is %llu", GOLDEN_RATIO_PRIME);
+       printk(KERN_INFO "OS -- Loading Module\n");
+       printk(KERN_INFO "%llu", GOLDEN_RATIO_PRIME);
+       printk(KERN_INFO "%llu", jiffies);
        return 0;
 }
 
 /* This function is called when the module is removed. */
 static void simple_exit(void) {
     printk(KERN_INFO "%lu\n", gcd(3300,40));
-    printk(KERN_INFO "Removing Module\n");
+    printk(KERN_INFO "OS -- Removing Module\n");
 }
 
 /* Macros for registering module entry and exit points. */
