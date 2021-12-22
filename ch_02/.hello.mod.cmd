43a232d4b75482310b116acd3e5fca6a7edb517e
drocpdp <davidreynon@gmail.com>
2021-12-22 09:37:58 +0100

    changes for current kernel

---
+++ ch_02/.hello.mod.cmd
@@ -0,0 +1,1 @@
+cmd_/home/drocpdp/Projects/silberschatz_solutions/ch_02/hello.mod := { echo  /home/drocpdp/Projects/silberschatz_solutions/ch_02/hello.o;  echo; } > /home/drocpdp/Projects/silberschatz_solutions/ch_02/hello.mod
